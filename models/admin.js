dc775438524f3b59e1e51cf8e781a6d2565374df
KAMLESH SATPUTE <kamleshsatpute51@gmail.com>
2025-09-19 00:38:17 +0530

    verify auth

--- models/admin.js
+++ models/admin.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 const adminSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
@@ -7,20 +6,6 @@ const adminSchema = new mongoose.Schema({
     password: { type: String, required: true },
     role: { type: String, enum: ['superadmin', 'admin'], default: 'admin' },
 }, { timestamps: true });
-
-
-adminSchema.pre('save', async function(next) {
-    const user = this;
-    if (!user.isModified('password')) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        const hashedpassword = await bcrypt.hash(user.password, salt);
-        user.password = hashedpassword;
-        next();
-    } catch (error) {
-        return next(error);
-    }
-});
 
 const Admin = mongoose.model('Admin', adminSchema);
 
--- server.js
+++ server.js
@@ -1,35 +1,41 @@
-const express=require('express');
-const dotenv=require('dotenv');
-const db=require('./db');
-const authMiddleware=require('./middlewares/authmiddleware');
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const db = require('./db');
+const authMiddleware = require('./middlewares/authmiddleware');
+
 dotenv.config();
 
-const app=express();
+const app = express();
+
+const corsOptions = {
+    origin: "http://localhost:3000",
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-const adminRoutes=require('./routes/adminroute');
-const teacherRoutes=require('./routes/teacherRoutes');
-const classroomRoutes=require('./routes/classroomRoutes');
-const divisionRoutes=require('./routes/divisionRoutes');
-const subjectRoutes=require('./routes/subjectRoutes');
-const settingsRoutes=require('./routes/settingsRoutes');
+const adminRoutes = require('./routes/adminroute');
+const teacherRoutes = require('./routes/teacherRoutes');
+const classroomRoutes = require('./routes/classroomRoutes');
+const divisionRoutes = require('./routes/divisionRoutes');
+const subjectRoutes = require('./routes/subjectRoutes');
+const settingsRoutes = require('./routes/settingsRoutes');
 
-app.use('/admin',adminRoutes);
-app.use('/teacher',authMiddleware,teacherRoutes);
-app.use('/classroom',authMiddleware,classroomRoutes);
-app.use('/division',authMiddleware,divisionRoutes);
-app.use('/subject',authMiddleware,subjectRoutes);
-app.use('/settings',authMiddleware,settingsRoutes);
+app.use('/admin', adminRoutes);
+app.use('/teacher', authMiddleware, teacherRoutes);
+app.use('/classroom', authMiddleware, classroomRoutes);
+app.use('/division', authMiddleware, divisionRoutes);
+app.use('/subject', authMiddleware, subjectRoutes);
+app.use('/settings', authMiddleware, settingsRoutes);
 
-
-
-const PORT=process.env.PORT||5000;
-app.listen(PORT,()=>{
-console.log(`Server listening on port ${PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
 
-module.exports=app;
-
-
+module.exports = app;
